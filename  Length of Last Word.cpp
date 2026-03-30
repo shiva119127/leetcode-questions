c88474a77a3614d21075e10460341661b75dcf72
shiva119127 <shiva.119127@gmail.com>
2026-03-31 02:52:47 +0530

    new commit

---
+++  Length of Last Word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.length();
+        int count = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == ' ' && count == 0) {
+                continue;
+            }
+            else if(s[i] != ' ') {
+                count++;
+            }
+            else {
+                break; 
+            }
+        }
+        return count;
+    }
+};
