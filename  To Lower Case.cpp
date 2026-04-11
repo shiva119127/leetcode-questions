c983e2a6469c8b6c3b827d17aa485dfe46168633
shiva119127 <shiva.119127@gmail.com>
2026-04-11 21:22:03 +0530

    new commit

---
+++  To Lower Case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+         for(int i = 0; i < s.size(); i++){
+       
+     if(s[i] <= 'Z' && s[i] >= 'A'){
+            s[i] = s[i]+32;
+          }   
+         }
+         return s;
+    }
+};
